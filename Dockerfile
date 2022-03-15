6bc0ec2867e264ea26344a550ad9dd78945ec332
Kundjanasith Thonglek <thonglek.kundjanasith.ti7@is.naist.jp>
2022-03-15 16:22:09 +0900

    isOpen

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ RUN apt-get upgrade
 RUN apt-get update
 RUN apt-get -y install apt-utils
 RUN apt-get -y install iputils-ping
+RUN apt-get -y install telnet
+RUN apt-get -y install python3
 
 EXPOSE 19190
 EXPOSE 19191
--- learning/main.py
+++ learning/main.py
@@ -5,6 +5,15 @@ def internet_on(ip):
     response = os.system("ping -c 1 " + hostname)   
     return response
 
+def isOpen(ip,port):
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   try:
+      s.connect((ip, int(port)))
+      s.shutdown(2)
+      return True
+   except:
+      return False
+
 config = configparser.ConfigParser()
 config.read('../config.ini')
 print(config.sections())
@@ -15,8 +24,8 @@ list_config_ip.pop(0)
 list_config_ip.pop(int(node)-1)
 for i in list_config_ip:
     ip = config['network'][i]
-    response = internet_on(ip)
-    if response == 0:
+    response = isOpen(ip,19190)
+    if response:
         print(ip,'is up!')
     else:
         print(ip,'is down!')
