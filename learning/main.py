b70f5ff6800c514bafbb1664ae9dfc26653e618d
Tem <tem@Tems-MacBook-Pro.local>
2022-03-22 04:29:08 +0900

    enable main

--- learning/main.py
+++ learning/main.py
@@ -55,7 +55,6 @@ for i in range(NUM_OF_ROUNDS):
         os.system('python3 ./transfer/client.py '+ip+' ./client/models/round_'+str(i+1)+'.h5')
     time.sleep(10) #Delay for storing the local model
     os.system('python3 server/aggregate.py '+str(i+1))
-    break
         
 
 
