9446f8ddee5bc29621c253121b8d644449755bf7
Kundjanasith Thonglek <thonglek.kundjanasith.ti7@is.naist.jp>
2022-03-22 22:10:08 +0900

    update model on server side

--- learning/server/model.py
+++ learning/server/model.py
@@ -1,10 +1,11 @@
 from tensorflow.keras.applications.mobilenet import MobileNet
+from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.layers import Input, Dense, BatchNormalization, Flatten
 from tensorflow.keras import Model
 from tensorflow.keras.optimizers import SGD
 
 def init():
-    model = MobileNet(include_top=True,input_tensor=Input(shape=(32,32,3)))
+    model = VGG16(include_top=False,input_tensor=Input(shape=(32,32,3)))
     x = model.output
     x = Flatten()(x)
     x = Dense(512,activation='relu')(x)
